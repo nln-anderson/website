c5e9d2d47f4f0a87379c4fd6aff1e49a4a629c12
nln-anderson <151287053+nln-anderson@users.noreply.github.com>
2025-01-09 15:51:00 -0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,20 +1,20 @@
 /* This creates the students table */
 
-TABLE students(
+CREATE TABLE students(
   id SERIAL PRIMARY KEY,
   first_name TEXT,
   last_name TEXT,
   math_level TEXT,
   grade_level INTEGER,
-  school TEXT)
+  school TEXT);
 
 /* This creates the sessions table */
 
-TABLE sessions(
+CREATE TABLE sessions(
   id SERIAL PRIMARY KEY,
   student_id INT,
   hours NUMERIC(2,1),
   rate INT,
   date DATE DEFAULT CURRENT_DATE,
   location TEXT,
-  paid BOOLEAN)
+  paid BOOLEAN);
