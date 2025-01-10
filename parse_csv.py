2a5df9fdfd18da99ba3fab0b59f8be512d40d045
nanderson-sdcc <noln.anderson@gmail.com>
2025-01-09 16:38:46 -0800

    more stuff

--- parse_csv.py
+++ parse_csv.py
@@ -12,7 +12,7 @@ def date_handler(date: str) -> str:
         new_date = f"{date_components[2]}/{date_components[0]}/{date_components[1]}"
     return new_date
 
-def rate_handler(name: str) -> int:
+def rate_handler(name: str, first: str) -> int:
     if name == "Kerekes" or name == "Woz" or name == "Boxer" or name=="Ramirez":
         return 35
     elif name == "Anastasi"or name == "McLeod" or name == "Eastepp":
@@ -22,6 +22,15 @@ def rate_handler(name: str) -> int:
     elif name == "Perez" or name == "Johnson" or name == "Clarke" or name=="Pasquini":
         return 60
     
+def content_handler(last_name: str, first_name: str) -> str:
+    if first_name == "Joseph" or first_name == "Ryan":
+        return "Algebra 2"
+    elif first_name in ("Sasha", "Ali", "Katheryn", "Sophie", "Tallulah"):
+        return "Calculus 1"
+    elif first_name in ("Aubrey", "Isabella", "Carly"):
+        return "College Algebra"
+    elif first_name in ("Nathan", "Kyla"):
+        return "Geometry"
 
 def main():
     with open(file_path, "r") as file:
@@ -31,7 +40,7 @@ def main():
             first_name = components[0].split()[0]
             last_name = components[0].split()[1]
             date = date_handler(components[1])
-            rate = rate_handler(last_name)
+            rate = rate_handler(last_name, first_name)
             paid = True
             
 
